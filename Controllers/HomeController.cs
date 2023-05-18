1f324583464cf94dc83db5b6bf3c3cb019448089
SujarHada <sujarhadaofficail@gmail.com>
2023-05-18 18:52:28 +0545

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        string baseURL = "https://localhost:7127/api/";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+     
+
+        public async Task<IActionResult> Index()
+        {
+            IList<UserEntity> users = new List<UserEntity>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("Contacts");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string results = await response.Content.ReadAsStringAsync();
+                    users = JsonConvert.DeserializeObject<List<UserEntity>>(results);
+                }
+                else
+                {
+                    Console.WriteLine("Error calling web API");
+                }
+            }
+
+            return View(users);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(UserEntity user)
+        {
+            UserEntity obj = new UserEntity()
+            {
+                fullName = user.fullName,
+                email = user.email,
+                phone = user.phone,
+                address = user.address
+            };
+
+            if (user.fullName != null)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(baseURL + "Contacts");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    HttpResponseMessage response = await client.PostAsJsonAsync("", obj);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error calling web API");
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumeWebAPI.Models
+{
+    public class UserEntity
+    {
+        public string fullName { get; set; } 
+        public string email { get; set; }
+        public int phone { get; set; }
+        public string address { get; set; }
+
+    }
+}
